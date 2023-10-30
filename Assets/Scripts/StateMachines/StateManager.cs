8002deecc65603d23a6982b427c6614b9e60bf7c
Charlie Mao <118494216+maocharliemao@users.noreply.github.com>
2023-10-30 16:30:59 +1100

    anchored the UI

--- Assets/Scripts/StateMachines/StateManager.cs
+++ Assets/Scripts/StateMachines/StateManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateManager : StateBases
+public class StateManager : MonoBehaviour
+    
 {
     public StateBases startingState;
     public StateBases currentState;
