47b8fa2fc03c7347e64f1b9e61735dcaca1da956
Charlie Mao <118494216+maocharliemao@users.noreply.github.com>
2023-12-11 13:39:59 +1100

    car stuff

--- Assets/Scripts/Referee.cs
+++ Assets/Scripts/Referee.cs
@@ -1,25 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Charlie;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Referee : MonoBehaviour
 {
-    public int redTeamScore  = 0;
-    public int blueTeamScore  = 0;
-    
-    public GameObject ball; 
-    public Rigidbody rb;
-    public delegate void ScoreChangeEvent(int redScore, int blueScore);
-    public event ScoreChangeEvent OnScoreChange;
-    
+    public int redTeamScore = 0;
+    public int blueTeamScore = 0;
+
+    public GameObject ball;
 
     
-    public delegate void GameOverEvent(int redScore, int blueScore);
-    public event GameOverEvent OnGameOver;
+    public GameObject TaxiPrefab;
+    public GameObject DororianPrefab;
+    public PlayerInputManager PlayerInputManager;
+    public GameObject cameraPrefab;
+    public Transform[] spawnPoints;
+    public List<CharliesCar> players;
     
+    public delegate void ScoreChangeEvent(int redScore, int blueScore);
+
+    public event ScoreChangeEvent OnScoreChange;
+
+
+    public delegate void GameOverEvent(int redScore, int blueScore);
+
+    public event GameOverEvent OnGameOver;
+
     public delegate void ResetToInitial();
+
     public event ResetToInitial OnPlayerReset;
 
+    
+    public void Awake()
+    {
+        if (PlayerInputManager != null) PlayerInputManager.onPlayerJoined += PlayerInputManagerOnonPlayerJoined;
+    }
+
+    private void PlayerInputManagerOnonPlayerJoined(PlayerInput aObj)
+    {
+        GameObject newCameraGo = Instantiate(cameraPrefab);
+
+        aObj.camera = newCameraGo.GetComponent<Camera>();
+        newCameraGo.GetComponent<CameraTracker>().target = aObj.transform;
+
+        if (spawnPoints.Length > 0)
+        {
+            aObj.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+        }
+
+        players.Add(aObj.gameObject.GetComponent<CharliesCar>());
+    }
+    
+    
+    
 
     private void ResetBall()
     {
@@ -27,7 +62,7 @@ public class Referee : MonoBehaviour
         {
             Debug.Log("Resetting the ball");
             ball.transform.position = Vector3.zero;
-            
+
             Rigidbody rb = ball.GetComponent<Rigidbody>();
             if (rb != null)
             {
@@ -36,7 +71,7 @@ public class Referee : MonoBehaviour
             }
         }
     }
-    
+
 
     public void AddScoreToRedTeam()
     {
@@ -55,14 +90,14 @@ public class Referee : MonoBehaviour
         GameOver();
         PlayerResetEvent();
     }
-    
+
     private void NotifyScoreChange()
     {
         Debug.Log("Notify");
         OnScoreChange?.Invoke(redTeamScore, blueTeamScore);
     }
 
-    
+
     private void GameOver()
     {
         Debug.Log("game over");
@@ -74,9 +109,4 @@ public class Referee : MonoBehaviour
         Debug.Log("reset");
         OnPlayerReset?.Invoke();
     }
-    
-    
-    
-    
-
 }
